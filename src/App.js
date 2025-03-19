ca949c4b98021333ce4d2e66dba6437b76849f2b
kp-04z <kijrl.pagtulon-an@student.ateneo.edu>
2025-03-19 20:16:26 +0800

    created barebones prototype

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
 import './App.css';
+import Button from './buttons/Button.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useState } from 'react';
+import { cloneElement } from 'react';
+
+var ingredients_list = [
+	'Cabbage',
+	'Garlic',
+	'Apple',
+	'Tomato',
+	'Water'
+	]
+
+var ingredients = []
+
+for (var key = 0; key < ingredients_list.length; key++) {
+	ingredients[key] = {name: ingredients_list[key], id: key, display: false}
 }
 
-export default App;
+export default function MainApp() {	
+	const [count, setCount] = useState(0)
+	const [values, setValues] = useState(ingredients)
+	const [list, setList] = useState([])
+	
+	function changeIngredient(steps) {
+		let temp_count = count + steps
+		
+		if (temp_count < 0) {
+			setCount(temp_count + ingredients.length)
+		} else if (temp_count > ingredients.length - 1) {
+			setCount(temp_count - ingredients.length)
+		} else {
+			setCount(temp_count)
+		}
+		
+		setValues(ingredients)
+	}
+	
+	function changeList(add) {		
+		if (!add) {
+			let added = list.concat(ingredients[count].name)
+			setList(added)
+			
+			ingredients[count].display = true
+		} else {			
+			let removed = list.filter(ingredient => ingredient !== ingredients[count].name)
+			setList(removed)
+			ingredients[count].display = false
+		}
+		setValues(ingredients)
+	}
+	
+	const display = list.map(ingredient =>
+		<div className="div">
+		<li> 
+			{ ingredient }
+		</li>
+		</div>
+	)
+	
+	return (
+	<>
+		<div className="div">
+			<h1> Food Words </h1>
+			<span>
+				<Button text="<" onClick={() => changeIngredient(-1)} />
+				<a className="a"> {count + 1 + ". " + values[count].name } </a>
+				<Button text=">" onClick={() => changeIngredient(1)}/>
+			</span> <br/> <br/>
+			
+			<Button text={values[count].display ? "Remove" : "Add"} onClick={() => changeList(values[count].display) }/> <br/> <br/>
+			
+			{list.length > 0 && (<h3> Added Ingredients </h3>)} 
+			<ol className="span">
+			{ display }
+			</ol>
+		</div>
+	</>
+	);
+}
---
+++ src/buttons/Button.js
@@ -0,0 +1,11 @@
+function Button({text, onClick}) {
+	return (
+	<>
+		<button className="button" onClick = {onClick}>
+			{ text }
+		</button>
+	</>
+	);
+}
+
+export default Button
